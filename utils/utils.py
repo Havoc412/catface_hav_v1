f8d28a365584763b9c6a12730f4eda3b951efc61
unknown <2993167370@qq.com>
2024-09-08 16:24:01 +0800

    update merge_breeds to filtrate the low conf.

--- utils/utils.py
+++ utils/utils.py
@@ -16,11 +16,12 @@ def cal_degree(pt1, pt2):
     return degrees
 
 
-def merge_breeds(breeds, proportion=None):
+def merge_breeds(breeds, proportion=None, eps=0.01):
     """
 
     :param breeds:
     :param proportion: conf 的 二级比重，默认都为 1
+    :param eps: conf 的最小值，太就没有数目参考价值了。
     :return:
     """
     # 初始化字典来存储累加的 conf 值和计数
@@ -35,11 +36,12 @@ def merge_breeds(breeds, proportion=None):
     # 重排
     sorted_conf = sorted(conf_accumulator.items(), key=lambda item: item[1]['total_conf'], reverse=True)
     # 提取排序后的 top5 和 conf
-    sorted_top5 = [item[0] for item in sorted_conf]
     if proportion is None:
         proportion = len(breeds)
-    sorted_conf_values = [item[1]['total_conf'] / proportion for item in sorted_conf]
-
+    sorted_conf_values = [item[1]['total_conf'] / proportion for item in sorted_conf
+                          if item[1]['total_conf'] / proportion > eps]  # 同时实现过滤概率过低的目标，降低后续的计算量。
+    sorted_top5 = [item[0] for idx, item in enumerate(sorted_conf)
+                   if idx < len(sorted_conf_values)]
     return {
         'top5': sorted_top5,
         'conf': sorted_conf_values
